a14b61ca60da16672232277e68bc6a01d00e141d
Pedro Hotz <pedrohotz2002@gmail.com>
2021-11-16 11:46:25 -0300

    fixingImports

--- src/components/login.js
+++ src/components/login.js
@@ -1,12 +1,12 @@
 
 import { Container,Header,StyledButton,StyledInput } from "../styles/sharedstyles";
 import {AiOutlineArrowLeft} from 'react-icons/ai';
-import { useState } from "react/cjs/react.development";
+import { useState } from "react";
 import {Link, useNavigate} from "react-router-dom";
 import Swal from "sweetalert2";
 import { submitLogin } from "../services/api.services";
 import { useContext } from "react";
-import UserContext from "../contexts/usercontext";
+import UserContext from "../contexts/usercontext.js";
 export default function Login(){
     const {setUser} = useContext(UserContext);
     const [email,setEmail] = useState('');
